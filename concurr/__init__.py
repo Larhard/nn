c79753a87aebc50622d3b75520d4721d17ca7b09
Larhard <larhard@gmail.com>
2015-02-22 16:58:39 +0100

    concurr: add gaussian transfer function

--- concurr/__init__.py
+++ concurr/__init__.py
@@ -2,6 +2,3 @@ from concurr.matrix import matrix_multiply
 from concurr.matrix import matrix_multiply_tn
 from concurr.matrix import matrix_add
 from concurr.matrix import matrix_sum
-
-from concurr.functions import sgm
-from concurr.functions import sgm_d
--- concurr/functions.py
+++ concurr/functions.py
@@ -33,11 +33,35 @@ void sgm_d(double *odata, double *idata, int n)
         odata[idx] = tmp;
     }
 }
+
+__global__
+void gaussian(double *odata, double *idata, int n)
+{
+    const int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+    if (idx < n) {
+        double x = idata[idx];
+        x = exp(-(x*x));
+        odata[idx] = x;
+    }
+}
+
+__global__
+void gaussian_d(double *odata, double *idata, int n)
+{
+    const int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+    if (idx < n) {
+        double x = idata[idx];
+        x = -2 * x * exp(-(x*x));
+        odata[idx] = x;
+    }
+}
 """)
 
 
 cuda_sgm = module.get_function('sgm')
 cuda_sgm_d = module.get_function('sgm_d')
+cuda_gaussian = module.get_function('gaussian')
+cuda_gaussian_d = module.get_function('gaussian_d')
 
 
 def sgm(idata):
@@ -65,6 +89,36 @@ def sgm_d(idata):
     block_dim, grid_dim = concurr.utils.get_dims_1d(n)
 
     cuda_sgm_d(odata, idata, np.int32(n),
+        block=block_dim, grid=grid_dim)
+
+    return odata
+
+
+def gaussian(idata):
+    if not isinstance(idata, gpuarray.GPUArray):
+        idata = gpuarray.to_gpu(np.ascontiguousarray(idata))
+
+    odata = gpuarray.GPUArray(idata.shape, dtype=np.float64)
+
+    n = functools.reduce(operator.mul, idata.shape)
+    block_dim, grid_dim = concurr.utils.get_dims_1d(n)
+
+    cuda_gaussian(odata, idata, np.int32(n),
+        block=block_dim, grid=grid_dim)
+
+    return odata
+
+
+def gaussian_d(idata):
+    if not isinstance(idata, gpuarray.GPUArray):
+        idata = gpuarray.to_gpu(np.ascontiguousarray(idata))
+
+    odata = gpuarray.GPUArray(idata.shape, dtype=np.float64)
+
+    n = functools.reduce(operator.mul, idata.shape)
+    block_dim, grid_dim = concurr.utils.get_dims_1d(n)
+
+    cuda_gaussian_d(odata, idata, np.int32(n),
         block=block_dim, grid=grid_dim)
 
     return odata
